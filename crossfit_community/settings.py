ffe9623bedc3674df67987b907fe3dc42915c791
MauRuRo <m.rubenderoos@gmail.com>
2020-12-21 16:13:51 +0000

    Adjust page layout and design for allauth login/register/logout pages

--- crossfit_community/settings.py
+++ crossfit_community/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'my_profile',
+    # Other
+    'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'crossfit_community.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     # { 
@@ -79,6 +84,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
