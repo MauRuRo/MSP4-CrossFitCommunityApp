4f18d80938fce28d19b99aa53f65eb3408675a03
MauRuRo <m.rubenderoos@gmail.com>
2021-01-26 19:54:33 +0000

    Tidy up, categorize and comment base.css

---
+++ home/static/home/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    console.log("connected")
+    $("body").css('background', "url('/media/background.jpg') no-repeat fixed")
+    $("body").css('background-size', "cover")
+
+})
