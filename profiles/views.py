bbb8907f4e87e590461686806acc0eccdd008c35
MauRuRo <m.rubenderoos@gmail.com>
2021-01-26 16:44:43 +0000

    Beautify views.py profiles and add comments

--- profiles/views.py
+++ profiles/views.py
@@ -28,11 +28,13 @@ import statistics
 
 #         return HttpResponse(content=e, status=400)
 
+
 def profile(request):
     """ a view to render the profile page, incl edit profileform"""
     if not request.user.is_authenticated:
         return render(request, 'home/index.html')
     try:
+        # Get all info needed to render Profile Page
         profile = UserProfile.objects.get(user=request.user)
         template = 'profiles/profile.html'
         profile = UserProfile.objects.get(user=request.user)
@@ -41,16 +43,38 @@ def profile(request):
         level_data = hero_levels.level_data
         general_level = hero_levels.general_level
         cat_levels = level_data
-        categories = ["Power Lifts", "Olympic Lifts", "Body Weight", "Heavy", "Light", "Long", "Speed", "Endurance"]
+        categories = [
+            "Power Lifts",
+            "Olympic Lifts",
+            "Body Weight",
+            "Heavy",
+            "Light",
+            "Long",
+            "Speed",
+            "Endurance"
+            ]
+        # Calculate user Stats.
         year_date = date.today() - timedelta(days=365)
         month_date = date.today() - timedelta(days=30)
         week_date = date.today() - timedelta(days=7)
-        user_logs_year = Log.objects.filter(user=request.user).filter(date__gt=year_date).count()
-        user_logs_month = Log.objects.filter(user=request.user).filter(date__gt=month_date).count()
-        user_logs_week = Log.objects.filter(user=request.user).filter(date__gt=week_date).count()
-        pry = Log.objects.filter(user=request.user).filter(date__gt=year_date).filter(personal_record=True).count()
-        prm = Log.objects.filter(user=request.user).filter(date__gt=month_date).filter(personal_record=True).count()
-        prw = Log.objects.filter(user=request.user).filter(date__gt=week_date).filter(personal_record=True).count()
+        user_logs_year = Log.objects.filter(
+            user=request.user
+            ).filter(date__gt=year_date).count()
+        user_logs_month = Log.objects.filter(
+            user=request.user
+            ).filter(date__gt=month_date).count()
+        user_logs_week = Log.objects.filter(
+            user=request.user
+            ).filter(date__gt=week_date).count()
+        pry = Log.objects.filter(user=request.user).filter(
+            date__gt=year_date
+            ).filter(personal_record=True).count()
+        prm = Log.objects.filter(user=request.user).filter(
+            date__gt=month_date
+            ).filter(personal_record=True).count()
+        prw = Log.objects.filter(user=request.user).filter(
+            date__gt=week_date
+            ).filter(personal_record=True).count()
         if (user_logs_year/12) > user_logs_month:
             perf_month = "low"
         else:
@@ -76,11 +100,13 @@ def profile(request):
         }
         return render(request, template, context)
     except UserProfile.DoesNotExist:
+        # If profile does not exist redirect to create_profile page.
         return redirect(reverse('create_profile'))
 
 
 def create_profile(request):
-    """A view to render the create profile page AND on POST upload a profile object to the database."""
+    """A view to render the create profile page
+    AND on POST upload a profile object to the database."""
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
     if request.method == "GET":
@@ -89,6 +115,7 @@ def create_profile(request):
             return redirect(reverse('profile'))
         else:
             profile_form = UserProfileForm()
+            # Set price for creating profile.
             stripe_total = 999
             stripe.api_key = stripe_secret_key
             intent = stripe.PaymentIntent.create(
@@ -107,11 +134,15 @@ def create_profile(request):
 
             return render(request, template, context)
     else:
+        # Get form data and create new UserProfile object.
         profile_form = UserProfileForm(request.POST, request.FILES)
         if profile_form.is_valid() and request.POST['date'] != '':
             pid = request.POST.get('client_secret').split('_secret')[0]
             new_profile = profile_form.save(commit=False)
-            new_profile.birthdate = datetime.strptime(request.POST.get('date'), "%d %b %Y")
+            new_profile.birthdate = datetime.strptime(
+                request.POST.get('date'),
+                "%d %b %Y"
+                )
             new_profile.stripe_pid = pid
             new_profile.email = request.user.email
             new_profile.user = request.user
@@ -119,9 +150,30 @@ def create_profile(request):
             hero_l = HeroLevels()
             hero_l.user = request.user
             hero_l.general_level = 0
-            hero_l.level_data = [{"cat": "Power Lifts", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}, {"cat": "Olympic Lifts", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}, {"cat": "Body Weight", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}, {"cat": "Heavy", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}, {"cat": "Light", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}, {"cat": "Long", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}, {"cat": "Speed", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}, {"cat": "Endurance", "perc": "none", "acc": "none", "wod_level": [{"wod": "none", "wodperc": "none", "wodpk": 0}]}]
+            # Set default level data for new Profile.
+            default_wodlevel = [{"wod": "none", "wodperc": "none", "wodpk": 0}]
+            hero_l.level_data = [
+                {"cat": "Power Lifts", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel},
+                {"cat": "Olympic Lifts", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel},
+                {"cat": "Body Weight", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel},
+                {"cat": "Heavy", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel},
+                {"cat": "Light", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel},
+                {"cat": "Long", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel},
+                {"cat": "Speed", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel},
+                {"cat": "Endurance", "perc": "none", "acc": "none",
+                    "wod_level": default_wodlevel}
+            ]
             hero_l.save()
-            messages.success(request, 'Profile succesfully created and payment succesfully processed! \
+            messages.success(
+                request,
+                'Profile succesfully created and payment succesfully processed! \
             Please explore and enjoy our digital hero community!')
             return redirect(reverse('profile'))
         else:
@@ -135,7 +187,10 @@ def edit_profile(request):
     """A function that edits a UserProfile object."""
     if request.user.is_authenticated:
         instance = UserProfile.objects.get(user=request.user)
-        profile_form = UserProfileForm(request.POST, request.FILES, instance=instance)
+        profile_form = UserProfileForm(
+            request.POST, request.FILES,
+            instance=instance
+            )
         if profile_form.is_valid():
             edited_profile = profile_form.save(commit=False)
             edited_profile.email = request.user.email
@@ -151,6 +206,8 @@ def edit_profile(request):
 
 def cat_levels_info(percentiles, cat_levels, cat, wod_level, wod_cat):
     """A helper function that returns Level information for a category"""
+    # Determine accuracy of averages dependent
+    # on amount of logs.
     if len(percentiles) >= 3:
         accuracy = "high"
     elif len(percentiles) == 2:
@@ -163,35 +220,40 @@ def cat_levels_info(percentiles, cat_levels, cat, wod_level, wod_cat):
         avg_percentile = round(statistics.mean(percentiles))
     else:
         avg_percentile = "none"
-    cat_levels.append({"cat": cat, "perc": avg_percentile, "acc": accuracy, "wod_level": wod_level})
+    cat_levels.append({
+        "cat": cat, "perc": avg_percentile,
+        "acc": accuracy, "wod_level": wod_level
+        })
     return cat_levels
 
 
 @require_POST
 def calc_level(request):
-    """A function that calculates and returns Levels, per WOD, per Category and General, incl. the relevant results and the accuracy of the assesment."""
+    """A function that calculates and returns Levels, per WOD,
+    per Category and General,
+    incl. the relevant results and the accuracy of the assesment."""
     if request.is_ajax():
-        #determine for which user the levels need to be calculated
+        # Determine for which user the levels need to be calculated
         if request.POST["user"] == "request":
             user = request.user
         else:
             user = User.objects.get(pk=request.POST["user"])
-        # create catagory list and 'reverse' dictionary for queries an initialize list for category levels
-        categories = ["Power Lifts", "Olympic Lifts", "Body Weight", "Heavy", "Light", "Long", "Speed", "Endurance"]
-        cat_reverse = {"Power Lifts": "PL", "Olympic Lifts": "OL", "Body Weight": "BW", "Heavy": "HE", "Light": "LI", "Long": "LO", "Speed": "SP", "Endurance": "EN"}
-        cat_levels = []
-        # loop through categories to calculate level per category
+        # loop through workouts to calculate level per workout and category.
         workouts = Workout.objects.all().order_by("workout_category")
+        cat_levels = []
         percentiles = []
         wod_level = []
         cat = ''
-        last_log = False
         for wod in workouts:
             wod_cat = wod.get_workout_category_display()
             if cat == '':
                 cat = wod_cat
             elif cat != wod_cat:
-                cat_levels = cat_levels_info(percentiles, cat_levels, cat, wod_level, wod_cat)
+                # If different cat then determine avg level for category
+                # and reset lists for the loop.
+                cat_levels = cat_levels_info(
+                    percentiles, cat_levels, cat, wod_level, wod_cat
+                    )
                 percentiles = []
                 wod_level = []
                 cat = wod_cat
@@ -199,9 +261,17 @@ def calc_level(request):
             percentile = data["percentile"]
             result = data["result"]
             if percentile is not None:
-                wod_level.append({"wod": wod.workout_name, "wodperc": percentile, "wodpk": wod.pk, "result": result})
+                wod_level.append({
+                    "wod": wod.workout_name,
+                    "wodperc": percentile,
+                    "wodpk": wod.pk,
+                    "result": result
+                    })
                 percentiles.append(percentile)
-        cat_levels = cat_levels_info(percentiles, cat_levels, cat, wod_level, wod_cat)
+        cat_levels = cat_levels_info(
+            percentiles, cat_levels, cat, wod_level, wod_cat
+            )
+        # Get category levels to determine general level average.
         avg_list = []
         for item in cat_levels:
             if item["perc"] != "none":
@@ -210,15 +280,16 @@ def calc_level(request):
             general_level = round(statistics.mean(avg_list))
         else:
             general_level = 0
+        # Update HeroLevel Object.
         level_data = HeroLevels.objects.filter(user=user)
         level_data.update(level_data=cat_levels)
         level_data.update(general_level=general_level)
         new_levels_html = loader.render_to_string(
-        'profiles/includes/herolevel.html',
-        {
-            "general_level": general_level,
-            "cat_levels": cat_levels
-        })
+            'profiles/includes/herolevel.html',
+            {
+                "general_level": general_level,
+                "cat_levels": cat_levels
+            })
         data = {
             'new_levels_html': new_levels_html
         }
@@ -229,18 +300,23 @@ def calc_level(request):
 
 
 def getLevels(user, wod):
-    """A helper function which returns the Level of a user for a particular workout and the relevant result."""
-    # check which date is exactly a year ago
+    """A helper function which returns the Level of
+    a user for a particular workout and the relevant result."""
+    # Check which date is exactly a year ago
     lapse_date = date.today() - timedelta(days=365)
-    if Log.objects.filter(user=user, workout=wod, rx=True, date__gt=lapse_date).count() == 0:
+    logcount = Log.objects.filter(
+        user=user, workout=wod, rx=True, date__gt=lapse_date).count()
+    # If no logs for this workout and this user then skip rest of function.
+    if logcount == 0:
         percentile = None
         user_result = None
         data = {
-        "percentile": percentile,
-        "result": user_result
+            "percentile": percentile,
+            "result": user_result
         }
         return data
     else:
+        # Determine workout type.
         ft = False
         amrap = False
         if wod.workout_type == 'FT':
@@ -254,37 +330,53 @@ def getLevels(user, wod):
         else:
             rank_result = 'mw_result'
             rank_result_order = '-mw_result'
-        all_logs = Log.objects.filter(user__userprofile__gender=user.userprofile.gender, workout=wod, rx=True, date__gt=lapse_date).order_by(f'{rank_result_order}')
+        # Get query of relevant logs.
+        all_logs = Log.objects.filter(
+            user__userprofile__gender=user.userprofile.gender,
+            workout=wod, rx=True,
+            date__gt=lapse_date).order_by(f'{rank_result_order}')
         all_logs_l = list(all_logs.values())
-        log_id_list =[]
+        log_id_list = []
         log_user_id_list = []
         user_result = ''
         user_index = False
         result_index = False
+        # Determine rank position of user,
+        # count members with better results,
+        # determine level for this workout.
         for log in all_logs_l:
             if not log["user_id"] in log_user_id_list:
                 log_user_id_list.append(log["user_id"])
                 log_id_list.append(log)
-                # If the log is for the requested user then get indexes and result
+                # If the log is for the requested user
+                # then get indexes and result
                 if log["user_id"] == user.pk:
                     user_result = str(log[f'{rank_result}'])
                     user_index = log_id_list.index(log)
-                # Get the index of the first result that's BETTER than the users result.
-                if ft and user_index and result_index == False:
+                # Get the index of the first result
+                # that's BETTER than the users result.
+                if ft and user_index and result_index is False:
                     for log in reversed(log_id_list):
-                        if log["ft_result"] < log_id_list[user_index]["ft_result"]:
+                        nextresult = log["ft_result"]
+                        userresult = log_id_list[user_index]["ft_result"]
+                        if nextresult < userresult:
                             result_index = log_id_list.index(log)
                             break
-                elif user_index and result_index == False:
+                elif user_index and result_index is False:
                     for log in reversed(log_id_list):
-                        if log[f'{rank_result}'] > log_id_list[user_index][f'{rank_result}']:
+                        nextresult = log[f'{rank_result}']
+                        userresult = log_id_list[user_index][f'{rank_result}']
+                        if nextresult > userresult:
                             result_index = log_id_list.index(log)
                             break
-        if result_index == False:
+        if result_index is False:
             result_index = 0
         result_index += 1
         total_results = len(log_id_list)
-        percentile = round((total_results - result_index)/total_results* 100)
+        percentile = round(
+            (total_results - result_index) / total_results * 100
+            )
+        # Adjust format of results, depending on type.
         if ft:
             while user_result[0] == "0" or user_result[0] == ":":
                 user_result = user_result[1:]
