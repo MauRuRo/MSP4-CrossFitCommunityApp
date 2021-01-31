4b596dadaac892edcdd4c153a91c81d1bd260695
MauRuRo <m.rubenderoos@gmail.com>
2021-01-31 21:09:37 +0000

    Partway through deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn crossfit_community.asgi:application
--- crossfit_community/settings.py
+++ crossfit_community/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['hero-community.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -154,13 +154,17 @@ ASGI_APPLICATION = 'crossfit_community.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
