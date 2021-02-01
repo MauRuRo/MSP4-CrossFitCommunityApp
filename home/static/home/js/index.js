f6da74818b9c875dd6a4f5d878c5b4fcd444ac6b
MauRuRo <m.rubenderoos@gmail.com>
2021-02-01 11:35:43 +0000

    reset some image sources

--- home/static/home/js/index.js
+++ home/static/home/js/index.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-    // reset background for landing page
-    // $("body").css('background', "url('/media/background.jpg') no-repeat fixed")
-    // $("body").css('background-size', "cover")
     $("body").addClass("indexbody")
 })
