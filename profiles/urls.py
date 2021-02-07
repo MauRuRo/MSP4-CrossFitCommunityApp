f33cbb89befe05836030bb96cc5c012ccdf44def
MauRuRo <m.rubenderoos@gmail.com>
2021-02-07 09:10:09 +0000

    fixing notifications deploy

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create_profile/', views.create_profile, name='create_profile'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('calc_level/', views.calc_level, name='calc_level'),
-    path('markAsRead/', views.markAsRead, name='markAsRead'),
+    # path('markAsRead/', views.markAsRead, name='markAsRead'),
     path('wh/', webhook, name="webhook"),
     path('cache_payment_create_profile/', views.cache_payment_create_profile, name="cache_payment_create_profile"),
 ]
