707c8bea1a5af2ec7a53fcf2b8820d4fb588df89
MauRuRo <m.rubenderoos@gmail.com>
2021-02-07 09:02:51 +0000

    fixing notifications deploy

--- crossfit_community/urls.py
+++ crossfit_community/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls import url
-# from django.conf.urls.static import static
-import notifications.urls
+from django.conf.urls.static import static
+# import notifications.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
